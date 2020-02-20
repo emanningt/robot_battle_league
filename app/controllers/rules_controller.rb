380f52a95fb87b06868006003ef2399f47a358cf
emanningt <emanningmusic@yahoo.com>
2020-02-19 20:49:21 -0800

    How to play is set up

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -8,6 +8,19 @@ class RulesController < ApplicationController
         erb :'/rules/Goal'
     end
 
+    get '/game_setup' do 
+        erb :'/rules/setup'
+    end 
+
+    get '/gameplay' do 
+        erb :'/rules/gameplay'
+    end 
+
+    get '/altrules' do 
+        erb :'/rules/alter_rules'
+    end 
+
+
 end 
 
 
